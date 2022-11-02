70018cea517591a946433a311a54c269b27ce96a
ionzuu <99742932+ionzuu@users.noreply.github.com>
2022-11-02 12:39:27 -0600

    Cambios a diseño de modal 0.9.2

--- src/structure/Popup/index.tsx
+++ src/structure/Popup/index.tsx
@@ -8,11 +8,14 @@ export const PopUp = () => {
         <div className={scss.modal}>
             <div className={scss.header}>
               <h2 className={scss.title}>Codigos Postales</h2>
-              <button className={scss.close}>x</button>
+              <button className={scss.close}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+</svg>
+</button>
             </div>
             <div className={scss.body}><p>Datos de Codigo Postal</p></div>
             <div className={scss.footer}>
-              <button className={scss.accept}>Aceptar</button>
+              <button className={scss.accept} disabled={true}>Aceptar</button>
             </div>
         </div>
             
