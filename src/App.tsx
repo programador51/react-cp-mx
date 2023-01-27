b02adff523410aea53361796537574d0376dc6f4
programador51 <gskitcat@gmail.com>
2023-01-26 18:38:39 -0600

    fix(removed remaining css files): removed scss and css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
 import React from "react";
-import "./App.css";
-import "./index.css";
-
 import { ModalCp } from "./structure/modal";
 
 function App() {
-  return (
-    <div>
-      <ModalCp />
-      <p
-        style={{
-          fontFamily: "var(--NunitoSans)",
-        }}
-      >
-        Hola
-      </p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
